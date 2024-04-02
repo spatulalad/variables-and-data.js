d1b7511a7518e6f2a6fd1fcd54547fe98fe39717
spatulalad <165229888+spatulalad@users.noreply.github.com>
2024-04-01 22:26:13 -0600

    Create variables-and-data.js

---
+++ variables-and-data.js
@@ -0,0 +1,25 @@
+/* 
+* Variables, Data Types, and Typing
+*/
+let wordData = "Word list";
+
+console.log (wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+primitive: boolean, null, undefined (don't set things to this yourself), number, strings ("blah blah541")
+composite: object
+
+let obj = {
+  key1: "value",
+  key2: 4,
+  boolean: true,
+  obj2: {
+    obj2Key1: "Internal Object Value",
+  }
+  
+};
+
+console.log(obj.key1);
