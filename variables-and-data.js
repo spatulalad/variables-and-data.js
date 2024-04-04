94305c9494438443a75c4edd2fe81610f0ce8fd8
Mike Athey <mathey@cnm.edu>
2024-04-04 10:18:12 -0600

    commiting on laptop

--- variables-and-data.js
+++ variables-and-data.js
@@ -11,6 +11,8 @@ let totalWithTax = tax + subTotal;
 
 console.log (totalWithTax);
 
+
+
 /*
 //Data Types
 //primitive: boolean, null, undefined, number, strings ("blah blah541")
@@ -29,3 +31,27 @@ let obj = {
 
 console.log(obj.key1);
 
+console.log(obj.obj2.obj2Key1)
+
+/*
+//Typing Systems
+ */
+//let variable : string = "string";
+
+/*
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+*/
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2"
+
+console.log(string);
